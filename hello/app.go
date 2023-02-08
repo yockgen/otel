09ac67cd1af8fd1c23e1e6448f8983fbaaaaf9ff
Mah Yock Gen <yockgen@gmail.com>
2023-02-08 16:56:53 +0800

    first commit
    Signed-off-by: Mah Yock Gen <yockgen@gmail.com>

---
+++ hello/app.go
@@ -0,0 +1,85 @@
+package main
+
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+        "strconv"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
+)
+
+const myAppNm = "yockgen"
+
+// App is a Fibonacci computation application.
+type App struct {
+	r io.Reader
+	l *log.Logger
+}
+
+// NewApp returns a new App.
+func NewApp(r io.Reader, l *log.Logger) *App {
+	return &App{r: r, l: l}
+}
+
+// Run starts polling users for Fibonacci number requests and writes results.
+func (a *App) MyAppRun(ctx context.Context) error {
+
+       for {
+		// Each execution of the run loop, we should get a new "root" span and context.
+		newCtx, span := otel.Tracer(myAppNm).Start(ctx, "MyAppRun")
+
+		n, err := a.Poll(newCtx)
+		if err != nil {
+			span.End()
+			return err
+		}
+
+		a.Write(newCtx, n)
+		span.End()
+	}
+
+}
+
+// Poll asks a user for input and returns the request.
+func (a *App) Poll(ctx context.Context) (int, error) {
+
+        _, span := otel.Tracer(myAppNm).Start(ctx, "Poll")
+	defer span.End()
+
+
+	a.l.Print("What Factoria number would you like to know: ")
+
+	var n int
+	_, err := fmt.Fscanf(a.r, "%d\n", &n)
+
+	// Store n as a string to not overflow an int64.
+	nStr := strconv.FormatUint(uint64(n), 10)
+	span.SetAttributes(attribute.String("request.n", nStr))
+
+	return n, err
+}
+
+// Write writes the n-th Fibonacci number back to the user.
+func (a *App) Write(ctx context.Context, n int) {
+
+        var span trace.Span
+	ctx, span = otel.Tracer(myAppNm).Start(ctx, "Write")
+	defer span.End()
+
+	f, err := func(ctx context.Context) (int, error) {
+		_, span := otel.Tracer(myAppNm).Start(ctx, "Factoria")
+		defer span.End()
+		return Factoria(n)
+	}(ctx)
+	if err != nil {
+		a.l.Printf("Factoria(%d): %v\n", n, err)
+	} else {
+		a.l.Printf("Factoria(%d) = %d\n", n, f)
+	}
+
+}
