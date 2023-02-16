b15e4eabf0e9266dbd517bc023f2130d55e1423b
Mah Yock Gen <yockgen@gmail.com>
2023-02-16 15:16:25 +0800

    Added nginx lb config and updated dynamic grafana dashboard creation script
    Signed-off-by: Mah Yock Gen <yockgen@gmail.com>

--- terraform/script/create_grafana_dbrd.py
+++ terraform/script/create_grafana_dbrd.py
@@ -5,14 +5,11 @@ grafana_api = GrafanaFace(
           host='192.168.1.107:3000'
           )
 
-
-
 _dbrd = grafana_api.search.search_dashboards(tag='test02')
 print (len(_dbrd))
 if len(_dbrd)>0:
     _itx = _dbrd[0]
     grafana_api.dashboard.delete_dashboard(dashboard_uid=_itx["uid"])
-
 
 pnl_01 = {'title':'Memory Usage on Node 1'}
 pnl_01["type"]="timeseries"
@@ -22,7 +19,6 @@ pnl_01_qry["datasource"]={'type':'influxdb','uid':'vbf3Km14z'}
 pnl_01_qry["query"] = "from(bucket: \"intel\")\r\n  |> range(start: -60m)\r\n  |> filter(fn: (r) =>\r\n    r._measurement == \"mem_used\" and r.host == \"192.168.1.107\"\r\n  )"
 pnl_01["targets"] = [pnl_01_qry]
 pnl_01["gridPos"] = {'h':8,'w':12,'x':0,'y':0}
-
 
 pnl_02 = {'title':'Memory Usage on Node 2'}
 pnl_02["type"]="timeseries"
@@ -34,5 +30,5 @@ pnl_02["targets"] = [pnl_02_qry]
 pnl_02["gridPos"] = {'h':8,'w':12,'x':14,'y':0}
 
 pnl_config = [pnl_01, pnl_02]
-grafana_api.dashboard.update_dashboard(dashboard={'dashboard': {'tags':['test02'], 'title': 'Test Intel 02','type':'dash-db','panels':pnl_config}, 'folderId': 0, 'overwrite': True})
+grafana_api.dashboard.update_dashboard(dashboard={'dashboard': {'tags':['test02'],'refresh':'5s', 'title': 'Test Intel 02','type':'dash-db','panels':pnl_config}, 'folderId': 0, 'overwrite': True})
 
