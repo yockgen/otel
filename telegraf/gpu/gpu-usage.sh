e5dc9b27b80d65883e1562ba3be20f0ec7614284
Mah Yock Gen <yock.gen.mah@intel.com>
2023-03-21 00:03:54 -0700

    Fixed intel-gpu-tools mapping issue between new and old vesion
    Signed-off-by: Mah Yock Gen <yock.gen.mah@intel.com>

--- telegraf/gpu/gpu-usage.sh
+++ telegraf/gpu/gpu-usage.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 result=$(timeout 3s intel_gpu_top -l | awk 'END{print}')
-render=$(echo $result | awk 'END{print $9}')
-blitter=$(echo $result | awk 'END{print $12}')
-video=$(echo $result | awk 'END{print $15}')
-videoEnh=$(echo $result | awk 'END{print $18}')
+ttl=$(echo $result | awk '{print NF}')
+
+if [ $ttl -ge 23 ] #intel-gpu-tools 1.26 onward version
+then
+  render=$(echo $result | awk 'END{print $9}')
+  blitter=$(echo $result | awk 'END{print $12}')
+  video=$(echo $result | awk 'END{print $15}')
+  videoEnh=$(echo $result | awk 'END{print $21}')
+else
+  render=$(echo $result | awk 'END{print $8}')
+  blitter=$(echo $result | awk 'END{print $11}')
+  video=$(echo $result | awk 'END{print $14}')
+  videoEnh=$(echo $result | awk 'END{print $17}')
+fi
 
 echo "{\"render3d\": ${render}, \"blitter\": ${blitter}, \"video\": ${video}, \"video_enhance\": ${videoEnh}}"
